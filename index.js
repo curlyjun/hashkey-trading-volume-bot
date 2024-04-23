f4317a9ae320e0d757c4e78b51e72e90471f1d85
curlyjun <tjdwns4850@gmail.com>
2024-04-23 23:41:01 +0900

    feat: install dotenv,change to npm

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
 import axios from "axios";
+import "dotenv/config";
+
+// constants
+
+/** Get Symbol Price Ticker */
+const GET_SYMBOL_PRICE =
+  "https://api-pro.sim.hashkeydev.com/quote/v1/ticker/price";
 
 // 비트코인 가격 가져오기
 async function getBTCUSD() {
   try {
-    const { data } = await axios.get(
-      "https://api-pro.sim.hashkeydev.com/quote/v1/ticker/price?symbol=BTCUSDC"
-    );
+    const url = `${GET_SYMBOL_PRICE}?symbol=BTCUSDC`;
+
+    const { data } = await axios.get(url);
     console.log(data);
     return data[0].p;
   } catch (error) {
@@ -15,6 +22,7 @@ async function getBTCUSD() {
 
 async function main() {
   const bitcoinPrice = await getBTCUSD();
+  console.log(process.env.API_KEY);
 }
 
 main();
